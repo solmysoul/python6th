16b77f6f9fd6d37edb5c8adc01f6e5324dcb789f
Hansol Lee <gksthf996@naver.com>
2023-05-30 11:24:26 +0900

    출력함수 실습

--- default.js
+++ default.js
@@ -1,1 +1,10 @@
-document.write('Hello World');
+document.write("Hello World");
+document.write("<h1>Welcome to JS Program</h1>")
+document.write("<h2>Welcome to JS program</h2>")
+
+console.log('Welcome JS program');
+console.info('Welcome JS program');
+console.warn('Welcome JS program');
+console.error('Welcome JS program');
+
+alert('Welcome JS Program');
