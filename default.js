bfed7ac29fa48b0f134130294a1bd423db8fccf1
Hansol Lee <gksthf996@naver.com>
2023-05-31 09:42:31 +0900

    크기비교

--- default.js
+++ default.js
@@ -1,8 +1,46 @@
 
-var cels = parseFloat(prompt("섭씨 입력: "));
-var farn = cels * (9 / 5) + 32;
+var num1 = parseInt(prompt('첫번째 숫자 입력 : '));
+var num2 = parseInt(prompt('두번째 숫자 입력 : '));
 
-document.write("화씨 : " + farn);
+if(num1 > num2) {
+    console.log("큰 수는 : " + num1);
+}
+if(num1 < num2) {
+    console.log("큰 수는 : " + num2);
+}
+
+
+
+
+//var num1 =20;
+//var num2 = 10;
+//var nem3 = "10";
+//var num4 = 20;
+//var num5 = 15;
+//
+//console.log('일반 크기비교')
+//console.log(nun1 > num2, num1, '>', num2);
+//console.log(num1 >= num2, num1, '>=', num2);
+//console.log(num1 < num2, num1, '<', num2);
+//console.log(num1 <= num2, num1, '<=', num2);
+//
+//console.log('같은지 여부 확인')
+//console.log(num1 == num4, num1, '==', num4);
+//console.log(num1 != num4, num1, '!=', num4);
+//
+//console.log('===')
+//console.log(num1 === num3, num1, '===', num3;
+//console.log(num2 === num3, num2, '===', num3;
+//console.log(num2 == num3, num2, '==', num3;
+//
+//console.log('num1 > num2 && num1 > num5: ', num1 > num2 && !(num1 > num5)); //false
+//console.log('num1 > num2 || num1 > num5: ', num1 > num2 || num1 > num5); //true
+
+
+//var cels = parseFloat(prompt("섭씨 입력: "));
+//var farn = cels * (9 / 5) + 32;
+//
+//document.write("화씨 : " + farn);
 
 
 
