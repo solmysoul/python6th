00c508c84625aac27b3ae2e857d8abf2913a92e4
Hansol Lee <gksthf996@naver.com>
2023-06-05 13:22:52 +0900

    할당 연산사 실습

--- main.py
+++ main.py
@@ -66,19 +66,41 @@
 #=======================
 
 #논리연산자
-a = 5
-b = 2
-c = 3
-d = 200
+# a = 5
+# b = 2
+# c = 3
+# d = 200
+#
+# print('AND 연산자')
+# print('a > b and a > c: ', a > b and a > c)
+#
+# print('OR 연산자')
+# print('a > b or a < c: ', a > b or a < c)
+#
+# print('NOT 연산자')
+# print('not(a < b): ', not(a < b))
 
-print('AND 연산자')
-print('a > b and a > c: ', a > b and a > c)
+#=======================
 
-print('OR 연산자')
-print('a > b or a < c: ', a > b or a < c)
+#할당 연산자 =
+a = 10
+b = 20
+m = 15
 
-print('NOT 연산자')
-print('not(a < b): ', not(a < b))
+y = a + b
+print(y)
+
+m += 10
+print(m)
+
+m **= 2
+print(m)
+
+m //= 10
+print(m)
+
+
+
 
 
 
