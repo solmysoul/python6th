31091900e2a9aa5ab6bb4c3a3900e62592a5c39d
Hansol Lee <gksthf996@naver.com>
2023-06-05 11:30:41 +0900

    산술 연산자 실습

--- main.py
+++ main.py
@@ -15,3 +15,43 @@ print(type(d))
 print(type(e))
 print(type(f))
 print(type(g))
+
+# 덧셈
+a = 4
+b = 2
+total = a + b
+print('total = a + b :', total)
+
+#뺄셈
+a = 4
+b = 2
+total = a - b
+print('total = a - b :', total)
+
+#곱셈
+total = a * b
+print('total = a * b :', total)
+
+#나눗셈
+print('a / b: ', a / b)
+
+#나머지
+a = 5
+b = 2
+print('a % b: ', a % b)
+
+#거듭제곱
+print('a ** b: ', a ** b)
+
+#몫(양수)
+print('a // b: ', a // b)
+
+#몫(음수)
+a = -5
+print('-a // b: ', a // b)
+
+
+
+
+
+
