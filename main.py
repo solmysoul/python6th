716d85bcd22fa84e649477952de61e19f4bfb638
Hansol Lee <gksthf996@naver.com>
2023-06-05 13:11:18 +0900

    비교 연산자 실습

--- main.py
+++ main.py
@@ -50,6 +50,19 @@ print('a // b: ', a // b)
 a = -5
 print('-a // b: ', a // b)
 
+#========================
+
+#비교연산자
+a = 5
+b = 2
+print('a < b: ', a < b)
+
+print('a <= b: ', a <= b)
+
+print('a != b: ', a != b)
+
+print('a == b: ', a == b)
+
 
 
 
