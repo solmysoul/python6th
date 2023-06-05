8cbb945434ae4b92292e7d139a070edee268da05
Hansol Lee <gksthf996@naver.com>
2023-06-05 13:19:32 +0900

    논리 연산자 실습

--- main.py
+++ main.py
@@ -1,67 +1,85 @@
 # 변수 선언
-a = 10
-b = 3.14
-c = "Hello, World!"
-d = [1, 2, 3]
-e = (4, 5, 6)
-f = {7, 8, 9}
-g = {"apple": 1, "banana": 2, "orange": 3}
+# a = 10
+# b = 3.14
+# c = "Hello, World!"
+# d = [1, 2, 3]
+# e = (4, 5, 6)
+# f = {7, 8, 9}
+# g = {"apple": 1, "banana": 2, "orange": 3}
+#
+# # 데이터 타입 출력
+# print(type(a))
+# print(type(b))
+# print(type(c))
+# print(type(d))
+# print(type(e))
+# print(type(f))
+# print(type(g))
+#
+# # 덧셈
+# a = 4
+# b = 2
+# total = a + b
+# print('total = a + b :', total)
+#
+# #뺄셈
+# a = 4
+# b = 2
+# total = a - b
+# print('total = a - b :', total)
+#
+# #곱셈
+# total = a * b
+# print('total = a * b :', total)
+#
+# #나눗셈
+# print('a / b: ', a / b)
+#
+# #나머지
+# a = 5
+# b = 2
+# print('a % b: ', a % b)
+#
+# #거듭제곱
+# print('a ** b: ', a ** b)
+#
+# #몫(양수)
+# print('a // b: ', a // b)
+#
+# #몫(음수)
+# a = -5
+# print('-a // b: ', a // b)
+#
+# #========================
+#
+# #비교연산자
+# a = 5
+# b = 2
+# print('a < b: ', a < b)
+#
+# print('a <= b: ', a <= b)
+#
+# print('a != b: ', a != b)
+#
+# print('a == b: ', a == b)
 
-# 데이터 타입 출력
-print(type(a))
-print(type(b))
-print(type(c))
-print(type(d))
-print(type(e))
-print(type(f))
-print(type(g))
+#=======================
 
-# 덧셈
-a = 4
-b = 2
-total = a + b
-print('total = a + b :', total)
-
-#뺄셈
-a = 4
-b = 2
-total = a - b
-print('total = a - b :', total)
-
-#곱셈
-total = a * b
-print('total = a * b :', total)
-
-#나눗셈
-print('a / b: ', a / b)
-
-#나머지
+#논리연산자
 a = 5
 b = 2
-print('a % b: ', a % b)
+c = 3
+d = 200
 
-#거듭제곱
-print('a ** b: ', a ** b)
+print('AND 연산자')
+print('a > b and a > c: ', a > b and a > c)
 
-#몫(양수)
-print('a // b: ', a // b)
+print('OR 연산자')
+print('a > b or a < c: ', a > b or a < c)
 
-#몫(음수)
-a = -5
-print('-a // b: ', a // b)
+print('NOT 연산자')
+print('not(a < b): ', not(a < b))
 
-#========================
-
-#비교연산자
-a = 5
-b = 2
-print('a < b: ', a < b)
-
-print('a <= b: ', a <= b)
-
-print('a != b: ', a != b)
-
-print('a == b: ', a == b)
 
 
 
