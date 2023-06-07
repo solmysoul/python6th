12a817e3b73f62c1148e2c2bdd367fc2b46b9b75
Hansol Lee <gksthf996@naver.com>
2023-06-07 14:21:54 +0900

    문자열 함수 실습

--- main.py
+++ main.py
@@ -539,40 +539,50 @@
 # for i in f:
 #     print(i)
 
-str1 = 'LikeLion'
-str2 = "LikeLion"
-str3 = '''
-동해물과 백두산이
-마르고 닳도록
-하느님이 보우하사
-우리나라 만세
-'''
-str4 = """
-동해물과 백두산이
-마르고 닳도록
-하느님이 보우하사
-우리나라 만세
-"""
+# str1 = 'LikeLion'
+# str2 = "LikeLion"
+# str3 = '''
+# 동해물과 백두산이
+# 마르고 닳도록
+# 하느님이 보우하사
+# 우리나라 만세
+# '''
+# str4 = """
+# 동해물과 백두산이
+# 마르고 닳도록
+# 하느님이 보우하사
+# 우리나라 만세
+# """
+#
+# print(str1)
+# print(str2)
+# print(str3)
+# print(str4)
+#
+# str5 = 'Hello "Like Lion" How are you'
+# str6 = "Hello 'Like Lion' How are you"
+#
+# print(str5)
+# print(str6)
+#
+# str7 = "Hello \nHow are you?"
+# str8 = "Hello \\nHow are you?"
+# str9 = r"Hello \nHow are you?"
+#
+# print(str7)
+# print(str8)
+# print(str9)
 
-print(str1)
-print(str2)
-print(str3)
-print(str4)
-
-str5 = 'Hello "Like Lion" How are you'
-str6 = "Hello 'Like Lion' How are you"
-
-print(str5)
-print(str6)
-
-str7 = "Hello \nHow are you?"
-str8 = "Hello \\nHow are you?"
-str9 = r"Hello \nHow are you?"
-
-print(str7)
-print(str8)
-print(str9)
-
+s = "Hello World"
+print(s.upper())
+print(s.lower())
+print(s.swapcase())
+print(s.title())
+print(s.isupper())
+print(s.islower())
+print(s.istitle())
+print(s.isdigit())
+print(s.isalpha())
 
 
 
