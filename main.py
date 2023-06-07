0895a2942c895f744e6ba817a3234867192b06e5
Hansol Lee <gksthf996@naver.com>
2023-06-07 10:28:23 +0900

    range 함수

--- main.py
+++ main.py
@@ -369,6 +369,36 @@
 #         j += 1
 # print("코드 종료 ")
 
+# range
+
+# for i in range(5):
+#     print(i)
+#
+# for i in range(2,7):
+#     print(i)
+
+# for i in range(1, 10, 2):
+#     print(i)
+
+# for i in range(-1, -10, -2):
+#     print(i)
+
+# a = range(5)
+# print(a[0])
+# print(a[1])
+# print(a[2])
+# print(a[3])
+# print(a[4])
+
+# print("Reverse Rage with Start, stop, step")
+# r = range(5, 0, -1)
+# print(r[0])
+# print(r[1])
+# print(r[2])
+# print(r[3])
+# print(r[4])
+
+
 
 
 
