05437f67ce693b0ee54b7345c92d09114aa2c975
Hansol Lee <gksthf996@naver.com>
2023-06-07 14:07:20 +0900

    문자열 실습

--- main.py
+++ main.py
@@ -498,46 +498,84 @@
 #     print(stu_roll[i])
 #     i += 1
 
-from array import *
-stu_roll = array('i', [101, 102, 103, 104, 105, 106, 107])
-print("배열 슬라이싱")
-print(stu_roll[2:3])
-print(stu_roll[0:])
-print(stu_roll[-2:])
+# from array import *
+# stu_roll = array('i', [101, 102, 103, 104, 105, 106, 107])
+# print("배열 슬라이싱")
+# print(stu_roll[2:3])
+# print(stu_roll[0:])
+# print(stu_roll[-2:])
+#
+# n = len(stu_roll)
+# for i in range(n):
+#     print(i, "=", stu_roll[i])
+#
+# print("1:5까지")
+# a = stu_roll[1:6]
+# for i in a:
+#     print(i)
+#
+# print("0번째 부터 끝까지")
+# b = stu_roll[0:]
+# for i in b:
+#     print(i)
+#
+# print("처음부터 5번째까지")
+# c = stu_roll[:6]
+# for i in c:
+#     print(i)
+#
+# print("마지막 요소 4개")
+# d = stu_roll[-4:]
+# for i in d:
+#     print(i)
+#
+# print("0부터 6번째까지 2개씩 건너 뛰어 출력")
+# e = stu_roll[0:7:2]
+# for i in e:
+#     print(i)
+#
+# print("마지막 5개의 요소 중 오른쪽으로부터 2개의 요소를 출력")
+# f = stu_roll[-5:-3]
+# for i in f:
+#     print(i)
 
-n = len(stu_roll)
-for i in range(n):
-    print(i, "=", stu_roll[i])
+str1 = 'LikeLion'
+str2 = "LikeLion"
+str3 = '''
+동해물과 백두산이
+마르고 닳도록
+하느님이 보우하사
+우리나라 만세
+'''
+str4 = """
+동해물과 백두산이
+마르고 닳도록
+하느님이 보우하사
+우리나라 만세
+"""
 
-print("1:5까지")
-a = stu_roll[1:6]
-for i in a:
-    print(i)
+print(str1)
+print(str2)
+print(str3)
+print(str4)
 
-print("0번째 부터 끝까지")
-b = stu_roll[0:]
-for i in b:
-    print(i)
+str5 = 'Hello "Like Lion" How are you'
+str6 = "Hello 'Like Lion' How are you"
 
-print("처음부터 5번째까지")
-c = stu_roll[:6]
-for i in c:
-    print(i)
+print(str5)
+print(str6)
 
-print("마지막 요소 4개")
-d = stu_roll[-4:]
-for i in d:
-    print(i)
+str7 = "Hello \nHow are you?"
+str8 = "Hello \\nHow are you?"
+str9 = r"Hello \nHow are you?"
 
-print("0부터 6번째까지 2개씩 건너 뛰어 출력")
-e = stu_roll[0:7:2]
-for i in e:
-    print(i)
+print(str7)
+print(str8)
+print(str9)
 
-print("마지막 5개의 요소 중 오른쪽으로부터 2개의 요소를 출력")
-f = stu_roll[-5:-3]
-for i in f:
-    print(i)
+
+
+
 
 
 
