9ba490008e3f825cf09cdba4f413fc6ea7b489a6
Hansol Lee <gksthf996@naver.com>
2023-06-07 15:28:59 +0900

    중첩 함수 실습

--- main.py
+++ main.py
@@ -623,45 +623,60 @@
 #     print(f"Formatted Output {x + y:5.2f}")
 # add(20)
 
-print("반환")
-def add():
-    x = 10
-    y = 20
-    c = x + y
-    return c
+# print("반환")
+# def add():
+#     x = 10
+#     y = 20
+#     c = x + y
+#     return c
+#
+#
+# sum1 = add()
+# print(sum1)
+#
+# def add2():
+#     x = 10
+#     y = 20
+#     return x + y
+#
+#
+# sum2 = add2()
+# print(sum2)
+#
+# def add(y):
+#     x = 10
+#     return x + y
+#
+#
+# sum3 = add(20)
+# print(sum3)
+#
+# def add(y):
+#     x = 10
+#     c = x + y
+#     d = y - x
+#     return c, d, 50
+#
+# sum4, sub1, a = add(20)
+# print(sum4)
+# print(sub1)
+# print(a)
 
+def disp():
+    def show():
+        print("Show Function")
+    print("Disp Function")
+    show()
 
-sum1 = add()
-print(sum1)
+disp()
 
-def add2():
-    x = 10
-    y = 20
-    return x + y
+def disp():
+    def show():
+        return "Show Function"
+    result = show() + "Disp Function"
+    return result
 
-
-sum2 = add2()
-print(sum2)
-
-def add(y):
-    x = 10
-    return x + y
-
-
-sum3 = add(20)
-print(sum3)
-
-def add(y):
-    x = 10
-    c = x + y
-    d = y - x
-    return c, d, 50
-
-sum4, sub1, a = add(20)
-print(sum4)
-print(sub1)
-print(a)
-
+print(disp())
 
 
 
