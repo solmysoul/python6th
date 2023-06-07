3e35d3b6e9a3e83ca1fe9c06417d127c6796ea6b
Hansol Lee <gksthf996@naver.com>
2023-06-07 14:24:41 +0900

    strip 실습

--- main.py
+++ main.py
@@ -573,16 +573,23 @@
 # print(str8)
 # print(str9)
 
-s = "Hello World"
-print(s.upper())
-print(s.lower())
-print(s.swapcase())
-print(s.title())
-print(s.isupper())
-print(s.islower())
-print(s.istitle())
-print(s.isdigit())
-print(s.isalpha())
+# s = "Hello World"
+# print(s.upper())
+# print(s.lower())
+# print(s.swapcase())
+# print(s.title())
+# print(s.isupper())
+# print(s.islower())
+# print(s.istitle())
+# print(s.isdigit())
+# print(s.isalpha())
+
+s = " Hello World "
+print(s.lstrip())
+print(s.rstrip())
+print(s.strip())
+
+
 
 
 
