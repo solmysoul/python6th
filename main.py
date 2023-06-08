287c88ec9c1dc048925e16abc58718d0cf0655f0
Hansol Lee <gksthf996@naver.com>
2023-06-08 13:12:33 +0900

    제네레이터 실습 2

--- main.py
+++ main.py
@@ -1,16 +1,13 @@
-def fibonacci(n):
-    a, b = 0, 1
-    for _ in range(n):
-        yield a # 예약어
-        a, b = b, a + b
+def generate_alphabet(start_letter, end_letter):
+    start = ord(start_letter)
+    end = ord(end_letter)
+    while start <= end:
+        yield chr(start)
+        start += 1
 
-runner = fibonacci(10)
 
-print(next(runner))
-print(next(runner))
-print(next(runner))
+runner = generate_alphabet('A', 'F')
 
-for num in runner:
-    print(num)
-
+for letter in runner:
+    print(letter)
 
