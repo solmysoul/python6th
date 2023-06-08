1c9d98abce3e2a2c6acfa68c08907b463f6bb78e
Hansol Lee <gksthf996@naver.com>
2023-06-08 11:25:20 +0900

    리스트 실습 2 -1

--- main.py
+++ main.py
@@ -7,15 +7,15 @@ print(grocery)
 numbers = [10, 5, 8, 1, 7]
 numbers.sort() # 오름차순 정리
 
-print(numbers)
+print("numbers: ", numbers)
 
 slice_numbers = numbers[1:4]
 
-print(slice_numbers)
+print("slice_numbers: ", slice_numbers)
 
 numbers_copy = numbers.copy()
 
-print(numbers_copy)
+print("numbers_copy: ", numbers_copy)
 
 numbers_copy.pop()
 
@@ -25,4 +25,4 @@ print("numbers_copy: ", numbers_copy)
 
 numbers_clone = numbers[:]
 
-print(numbers_clone)
+print("numbers_clone: ", numbers_clone)
