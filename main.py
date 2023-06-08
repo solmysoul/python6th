62efa51623987cb447b1b3d4112ae65793c69016
Hansol Lee <gksthf996@naver.com>
2023-06-08 15:30:18 +0900

    Call by Value

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-def val(lst):
-    print("Inside Function Before Append: ", lst, id(lst))
-    lst.append(4)
-    print("Inside Function After Append: ", lst, id(lst))
+def val(x):
+    print("Inside: ", x, id(x))
+    x += 1
+    print("Inside After: ", x, id(x))
 
-list = [1, 2, 3]
-print("Before Calling Function: ", list, id(list))
-val(list)
-print("After Calling Function: ", list, id(list))
+x = 10
+print("Before Calling: ", x, id(x))
+val(x)
+print("After Calling: ", x, id(x))
