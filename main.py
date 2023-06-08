c95a75a0345f2814b379a8dbcd8014e88997053d
Hansol Lee <gksthf996@naver.com>
2023-06-08 15:25:05 +0900

    Call by Reference 참조를 이용한 함수 호출

--- main.py
+++ main.py
@@ -1,51 +1,9 @@
-stu = {101: 'Kim', 102: 'Lee', 103: 'Hong'}
-fees = {'kim': 2000, 'lee': 3000, 'hong': 8000}
-print(stu[101])
-print(stu[102])
-print(stu[103])
+def val(lst):
+    print("Inside Function Before Append: ", lst, id(lst))
+    lst.append(4)
+    print("Inside Function After Append: ", lst, id(lst))
 
-print(fees['kim'])
-print(fees['lee'])
-print(fees['hong'])
-
-stu[102] = 'Python'
-print(stu[102])
-
-stu[104] = '멋쟁이사자'
-print(stu[104])
-
-del stu[102]
-print(stu)
-
-print(101 in stu)
-print(102 in stu)
-
-print(101 not in stu)
-print(102 not in stu)
-
-# stu.clear()
-# print(stu)
-
-new_stu = stu.copy()
-
-key = (101, 102, 103)
-value = '멋쟁이사자'
-new_stu = dict.fromkeys(key, value)
-print(new_stu)
-
-print(stu[101])
-print(stu.get(101))
-
-print(stu.items())
-print(stu.keys())
-print(stu.values())
-stu[104] = '멋쟁이사자'
-print(stu)
-stu.update({104: '멋쟁이사자2'})
-print(stu)
-stu.pop(104)
-print(stu)
-stu.setdefault(104, 'Park')
-print(stu)
-
-print(stu.popitem())
+list = [1, 2, 3]
+print("Before Calling Function: ", list, id(list))
+val(list)
+print("After Calling Function: ", list, id(list))
