23a389084f5f873f644b3b27705f8152256386a4
Hansol Lee <gksthf996@naver.com>
2023-06-08 14:22:25 +0900

    set 실습

--- main.py
+++ main.py
@@ -13,5 +13,22 @@ a.discard('멋쟁이사자') # remove와 동일하나 에러가 나지 않음
 print(a)
 
 new_set = a.copy()
-new_set.clear()
+# new_set.clear()
 print(new_set)
+
+intersection_a_new = a.intersection(new_set, a, b) # 교집합
+print(intersection_a_new)
+
+union_a = a.union(new_set) # 합집합
+print('union_a: ', union_a)
+
+difference_a = a.difference(new_set) #차집합
+print('difference_a: ', difference_a)
+
+print(b.issubset(a)) # 부분집합
+
+print(a.issuperset(b))
+
+sym_a = a.symmetric_difference(new_set) # 대칭차집합 / 교집합을 뺀 나머지
+print('symmetric_difference: ', sym_a)
+
