b2471d41eca935f89a3c6cdc4a863720f8be3109
Hansol Lee <gksthf996@naver.com>
2023-06-08 14:11:29 +0900

    기본 set 실습

--- main.py
+++ main.py
@@ -1,62 +1,17 @@
-b = (10)
-c = (10,)
+a = {10, 20, 30}
+a = {10, 20, 30, "멋쟁이사자", "hansol", 40}
+a = {10, 20, 30, 40, 10, "멋쟁이사자", 20}
+
+b = set()
 print(type(b))
-print(type(c))
 
-d = (10, 20, 30, 40)
-e = (10, 20, -50, 21.3, '멋쟁이사자')
-f = 10, 20, -50, 21.3, '멋쟁이사자'
+a.add(50)
+a.update([10, 20, 60, 70])
+print(a)
+a.remove('멋쟁이사자') # 지울 값이 없으면 에러가 남
+a.discard('멋쟁이사자') # remove와 동일하나 에러가 나지 않음
+print(a)
 
-print(d,e,f, sep="\n")
-
-print(f[0])
-print(f[1])
-print(f[2])
-print(f[3])
-print(f[4])
-
-print(f[:3])
-print(f[1:4])
-
-print(c + f)
-print(f * 3)
-
-print(10 in f)
-print(-10 in f)
-
-h = (10, 20, -50, 20)
-print(min(h), max(h))
-
-print(h.count(20))
-print(h.index(20))
-
-sorted_a = sorted(h)
-print(sorted_a)
-
-a = (10, 20, -50)
-x, y, z = a
-print(x, y, z)
-
-# a와 b의 변수 바꾸기
-
-a = 1
-b = 2
-
-tmp = a
-a = b
-b = tmp
-print(a, b)
-
-# 언패킹으로 변수 바꾸기
-
-a, b = b, a
-print(a, b)
-
-list_h = list(h)
-print(list_h, type(list_h))
-
-tuple_h = tuple(list_h)
-print(tuple_h, type(tuple_h))
-
-nested_tuple = ((1,2,3), (4,5,6), (7,8,9))
-print(nested_tuple)
+new_set = a.copy()
+new_set.clear()
+print(new_set)
