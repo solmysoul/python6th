3f00fcd67e7e6c98f124afb62b2dde0baba00e67
Hansol Lee <gksthf996@naver.com>
2023-06-09 13:24:41 +0900

    파일 실습 (열기, 닫기)

--- main.py
+++ main.py
@@ -1,19 +1,7 @@
-from datetime import timedelta
-from datetime import date
-from datetime import datetime
+file_object = open('example.txt', 'r')
 
-td = timedelta(days=10)
-print(td)
+content = file_object.read()
 
-d1 = date(year=2023, month=5, day=5)
-d2 = date(year=2023, month=6, day=9)
+print(content)
 
-print(d1 == d2) # 등호 연산자에 대한 재정의
-print(d1 < d2)
-print(d1 > d2)
-
-dt = datetime.today()
-
-formated_datetime = dt.strftime('%B, %d, %Y')
-print(formated_datetime)
-
+file_object.close()
