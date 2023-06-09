4b2ef3876ba809439e0c2f398392a78cb8026836
Hansol Lee <gksthf996@naver.com>
2023-06-09 11:25:47 +0900

    멘토님 추천 클래스 상속 실습

--- main.py
+++ main.py
@@ -1,26 +1,40 @@
-import time # import 에서 해당 기능을 가져옴
-from datetime import datetime
-from datetime import date
+class ParentClass:
+    def __init__(self):
+        self.name = 'parent'
+        self.number = 10
 
-print(time.time())
-print(time.ctime())
+    def __str__(self): # 프린트 할 때 예쁘게 출력해주는 함수
+        return f'ParentClass name : {self.name}, number : {self.number}'
+
+    def add_num(self, new_number):
+        print('부모 : ', new_number, '만큼 더해야지')
+        self.number = self.number + new_number
+
+class ChildClass (ParentClass): # 부모 클래스를 상속 받아서 실행함, 아무 입력을 하지 않으면 부모 클래스와 똑같이 출력
+    def __init__(self):
+        super().__init__() # 재정의, 이 코드가 없으면 오류남
+        self.name = 'child'
+
+    def __str__(self):
+        return f'ChildClass name : {self.name}, number : {self.number}'
+
+    def add_num(self, new_number):
+        print('말 안듣는 자식: 고정적으로 5 더할건데?')
+        self.number = self.number + 5
 
 
-dt = datetime(year=2023, month=5, day=5, hour=10, minute=30)
-print(dt)
-print(type(dt))
+parent = ParentClass()
+child = ChildClass()
+print(parent)
+print(child)
+print("=============")
 
-current_datetime = datetime.now()
-print(current_datetime)
+print('7을 더하세요')
+parent.add_num(7)
+child.add_num(7)
 
-current_ctime = time.ctime()
-print(current_ctime)
-
-d = date(year=2023, month=6, day=25) # 시간 정보 없이 날짜만
-print(d)
-
-current_date = date.today() # 시간 정보 없이 현재 날짜만
-print(current_date)
+print(parent)
+print(child)
 
 
 
