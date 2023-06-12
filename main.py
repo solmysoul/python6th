7592b999dd27e0f6efbc790e750caacfa56d5e1b
Hansol Lee <gksthf996@naver.com>
2023-06-12 10:20:25 +0900

    클래스 실습

--- main.py
+++ main.py
@@ -1,35 +1,24 @@
-import  os
-import time
+class Car:
+    # 클래스 속성
+    wheels = 4
 
-current_directory = os.getcwd() # 절대경로를 알려줌
-print(current_directory)
+    def __init__(self, make, model, color):
+        self.make = make
+        self.model = model
+        self.color = color
+    # Method
+    def drive(self):
+        return "The car is moving!"
 
-# os.mkdir('new_directory') # 파일추가
+    def stop(self):
+        return "The car has stopped."
 
-# os.makedirs('parent_directory/child_directory/grandchild_directory') # /를 통한 중첩 디렉토리
+my_car = Car("Kia", "Morning", "Blue")
 
-# os.chdir('new_directory')
-# current_directory2 = os.getcwd()
-# print(current_directory2)
+# 속성 사용
+print(my_car.make)
 
-# with open('example.txt','w') as file_object:
-#     file_object.write('Hello, World!')
-
-# os.rename('new_directory', 'new_directory2') # 이름 변경
-#
-# os.rmdir('new_directory2') # 폴더 한개 삭제
-#
-# os.removedirs('parent_directory/child_directory/grandchild_directory') # 폴더 여러개 삭제
-
-# os.makedirs('parent_directory/child_directory/grandchild_directory')
-
-# for dirpath, dirnames, filenames in os.walk('.'):
-#     print(f"디렉터리 경로: {dirpath}")
-#     print(f"디렉터리 이름: {dirnames}")
-#     print(f"파일이름 : {filenames}")
-
-for dirpath, dirnames, filenames in os.walk('parent_directory'):
-    print(f"디렉터리 경로: {dirpath}")
-    print(f"디렉터리 이름: {dirnames}")
-    print(f"파일이름 : {filenames}")
+# 메소드 호출
+print(my_car.drive())
+print(my_car.stop())
 
