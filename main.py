2785186c2187b1cfe5ca0fbd4cc585c3ed9d5e48
Hansol Lee <gksthf996@naver.com>
2023-06-12 13:04:39 +0900

    패키지 실습

--- main.py
+++ main.py
@@ -1,27 +1,8 @@
-# 다중 상속
-# 주의 사항 : 상속 받은 메소드 간의 이름 충돌
+# 패키지
+from python6th.MyApp.Handlers.text_handler import handle_text
 
-class Engine:
-    def start(self):
-        return "Engine started"
-
-    def stop(self):
-        return "Engine stopped"
+input_text = "python package practice"
+handle_text(input_text)
 
 
-class Wheels:
-    def rotate(self):
-        return "Wheels are rotating"
 
-
-# 다중 상속
-class Car(Engine, Wheels):
-    pass
-
-
-# 인스턴스 생성
-my_car = Car()
-
-# 부모 클래스의 메소드 사용
-print(my_car.start())
-print(my_car.rotate())
