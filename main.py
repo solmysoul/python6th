6269e6f3c2055944a143f5769b408c3705ac5398
Hansol Lee <gksthf996@naver.com>
2023-06-12 15:41:00 +0900

    locals, pprint 실습

--- main.py
+++ main.py
@@ -1,15 +1,7 @@
-a = ['a1', 'b2', 'c3']
+import pprint
 
-for i in range(len(a)):
-    print(i, a[i])
-print("==========")
-i = 0
-for v in a:
-    print(i, v)
-    i += 1
-print("==========")
-for i, v in enumerate(a):
-    print(i, v)
+pprint.pprint(locals())
+
 
 
 
