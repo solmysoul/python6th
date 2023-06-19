0ad28462ebd00e0dfcb7b369c8a6e5449572e85c
Hansol Lee <gksthf996@naver.com>
2023-06-19 15:45:39 +0900

    http.server 모듈 실습

--- main.py
+++ main.py
@@ -1,75 +1,14 @@
-# parser = 구문 분석 / 태그 제거
-import http.client
-from pathlib import Path
-from urllib.parse import urljoin, urlunparse
-from urllib.request import urlopen, urlretrieve
-from html.parser import HTMLParser
+from http.server import HTTPServer, BaseHTTPRequestHandler
 
+class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200, 'OK')
+        self.send_header('Content-Type', 'text/plain')
+        self.end_headers()
+        self.wfile.write(b"Hello World")
 
-class ImageParser(HTMLParser):
-    def __init__(self):
-        super().__init__()
-        self.result = []
-    def handle_starttag(self, tag, attrs):
-        if tag != 'img':
-            return
-        if not hasattr(self, 'result'):
-            self.result = []
-        for name, value in attrs:
-            if name == 'src':
-                self.result.append(value)
-
-def parse_image(data):
-    parser = ImageParser()
-    parser.feed(data)
-    dataSet = set(x for x in parser.result)
-    return dataSet
-
-def download_image(url, data):
-    download_dir = Path('DOWNLOAD')
-    download_dir.mkdir(exist_ok=True)
-
-    parser = ImageParser()
-    parser.feed(data)
-    data_set = set(x for x in parser.result)
-    for x in sorted(data_set):
-        image_url = urljoin(url, x)
-        basename = Path(image_url).name
-        target_file = download_dir / basename
-        print(target_file)
-
-        print("Downloading...", image_url)
-        urlretrieve(image_url, target_file)
-
-
-
-
-def main():
-    # url = 'https://google.co.kr'
-    # with urlopen(url) as f:
-    #     charset = f.headers.get_params('charset')[1][1]
-    #     data = f.read().decode(charset)
-    host = "www.google.co.kr"
-    conn = http.client.HTTPConnection(host)
-    conn.request('GET', '')
-    resp = conn.getresponse()
-
-    charset = resp.msg.get_param('charset')
-    print('charset:', charset)
-    data = resp.read().decode(charset)
-    conn.close()
-
-    print('\n>>>>> Downloading...', host)
-    url = urlunparse(('http', host, '', '', '', ''))
-
-
-    data_set = parse_image(data)
-    print('\n>>>>> Fetch Image from', url)
-    print('\n'.join(sorted(data_set)))
-    download_image(url, data)
-
-if __name__ == '__main__':
-    main()
+server = HTTPServer(("", 8080), SimpleHTTPRequestHandler)
+server.serve_forever()
 
 
 
