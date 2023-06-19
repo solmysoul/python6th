8b46d74e832947825ef98209662bc576498d937f
Hansol Lee <gksthf996@naver.com>
2023-06-19 14:44:22 +0900

    이미지 경로 추출 HTMLParser 실습

--- main.py
+++ main.py
@@ -1,13 +1,38 @@
-# 이미지 parser
-import urllib.request
+# parser = 구문 분석 / 태그 제거
+from urllib.request import urlopen
+from html.parser import HTMLParser
 
-url = 'https://www.google.com'
+class ImageParser(HTMLParser):
+    def __init__(self):
+        super().__init__()
+        self.result = []
+    def handle_starttag(self, tag, attrs):
+        if tag != 'img':
+            return
+        if not hasattr(self, 'result'):
+            self.result = []
+        for name, value in attrs:
+            if name == 'src':
+                self.result.append(value)
 
-request = urllib.request.Request(url)
-response = urllib.request.urlopen(request)
+def parse_image(data):
+    parser = ImageParser()
+    parser.feed(data)
+    dataSet = set(x for x in parser.result)
+    return dataSet
 
-html = response.read()
+def main():
+    url = 'https://google.co.kr'
+    with urlopen(url) as f:
+        charset = f.headers.get_params('charset')[1][1]
+        data = f.read().decode(charset)
 
-print(html)
+    data_set = parse_image(data)
+    print('\n>>>>> Fetch Image from', url)
+    print('\n'.join(sorted(data_set)))
+
+if __name__ == '__main__':
+    main()
+
 
 
