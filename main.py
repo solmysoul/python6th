85bcc5d90ea0f55c0717327d1a2a19379b4346aa
Hansol Lee <gksthf996@naver.com>
2023-06-19 15:32:18 +0900

    이미지 경로 추출 HTMLParser 실습2

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 # parser = 구문 분석 / 태그 제거
-from urllib.request import urlopen
+import http.client
+from pathlib import Path
+from urllib.parse import urljoin, urlunparse
+from urllib.request import urlopen, urlretrieve
 from html.parser import HTMLParser
+
 
 class ImageParser(HTMLParser):
     def __init__(self):
@@ -21,15 +25,48 @@ def parse_image(data):
     dataSet = set(x for x in parser.result)
     return dataSet
 
+def download_image(url, data):
+    download_dir = Path('DOWNLOAD')
+    download_dir.mkdir(exist_ok=True)
+
+    parser = ImageParser()
+    parser.feed(data)
+    data_set = set(x for x in parser.result)
+    for x in sorted(data_set):
+        image_url = urljoin(url, x)
+        basename = Path(image_url).name
+        target_file = download_dir / basename
+        print(target_file)
+
+        print("Downloading...", image_url)
+        urlretrieve(image_url, target_file)
+
+
+
+
 def main():
-    url = 'https://google.co.kr'
-    with urlopen(url) as f:
-        charset = f.headers.get_params('charset')[1][1]
-        data = f.read().decode(charset)
+    # url = 'https://google.co.kr'
+    # with urlopen(url) as f:
+    #     charset = f.headers.get_params('charset')[1][1]
+    #     data = f.read().decode(charset)
+    host = "www.google.co.kr"
+    conn = http.client.HTTPConnection(host)
+    conn.request('GET', '')
+    resp = conn.getresponse()
+
+    charset = resp.msg.get_param('charset')
+    print('charset:', charset)
+    data = resp.read().decode(charset)
+    conn.close()
+
+    print('\n>>>>> Downloading...', host)
+    url = urlunparse(('http', host, '', '', '', ''))
+
 
     data_set = parse_image(data)
     print('\n>>>>> Fetch Image from', url)
     print('\n'.join(sorted(data_set)))
+    download_image(url, data)
 
 if __name__ == '__main__':
     main()
