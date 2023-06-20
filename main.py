126bb639c128e4c5700ade0a21e2221d300364dd
Hansol Lee <gksthf996@naver.com>
2023-06-20 15:07:01 +0900

    db파일추가

--- main.py
+++ main.py
@@ -1,19 +1,1 @@
-from wsgiref.simple_server import make_server
-
-def application(environ, start_response):
-    response_body = b"Hello, World!" # 바이트로 변환
-    status = "200 OK"
-    headers = [("Content-type", "text/plain")]
-
-    start_response(status, headers)
-    return [response_body]
-
-if __name__ == '__main__':
-    httpd = make_server("", 8000, application)
-    print("Running...")
-    httpd.serve_forever()
-
-
-
-
 
