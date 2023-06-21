4cc1ac7aee4d9deed21e099ed049ddee1777834f
Hansol Lee <gksthf996@naver.com>
2023-06-21 15:14:58 +0900

    polls vote 함수 구현

--- myproject/urls.py
+++ myproject/urls.py
@@ -20,6 +20,7 @@ from django.urls import path, include
 from polls import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('polls/', include('polls.urls'))
+    path("admin/", admin.site.urls),
+    path('polls/', include('polls.urls')),
 ]
+
--- polls/models.py
+++ polls/models.py
@@ -1,18 +1,18 @@
 from django.db import models
 
+
 # Create your models here.
-# 모델이라는 특성을 상속 받은 클래스 생성
 class Question(models.Model):
-    object = None
+    objects = None
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
-    # 문자열로 변환해주는 함수
+
     def __str__(self):
         return self.question_text
 
-class Choice(models.Model): # 문항
+
+class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
-    # foreignKey Question과 연결
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
 
