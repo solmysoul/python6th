b0bcb80992b85be8fc66146a5c1835c5b990b5b2
Hansol Lee <gksthf996@naver.com>
2023-06-23 14:37:48 +0900

    apps.py 변경 실습

--- myproject/views.py
+++ myproject/views.py
@@ -1,10 +1,16 @@
 from django.views.generic import TemplateView
-
+from django.apps import apps
 
 class HomeView(TemplateView):
     template_name = 'home.html'
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['app_list'] = ['polls', 'books']
+        # context['app_list'] = ['polls', 'books']
+        dictVerbose = {}
+        for app in apps.get_app_configs():
+
+            if 'site-packages' not in app.path:
+                dictVerbose[app.label] = app.verbose_name
+        context['verbose dict'] = dictVerbose
         return context
